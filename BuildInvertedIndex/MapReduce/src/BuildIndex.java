cbf3a0c37ed233c0de554c6d735adebe83f3049e
wzz <908868432@qq.com>
2016-12-26 22:11:50 +0800

    mod mapreduce

--- BuildInvertedIndex/MapReduce/src/BuildIndex.java
+++ BuildInvertedIndex/MapReduce/src/BuildIndex.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -33,8 +34,14 @@ public class BuildIndex {
 
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             String documenmts = "";
+            HashMap<Text,IntWritable> map = new HashMap<Text,IntWritable>();
             for (Text val : values) {
-                documenmts =  val.toString()+"/"+documenmts;
+                if (map.containsKey(val)){
+                    continue;
+                }else{
+                    map.put(val,new IntWritable(1));
+                    documenmts =  val.toString()+"/"+documenmts;
+                }
             }
             result.set(documenmts);
             context.write(key, result);
@@ -43,9 +50,9 @@ public class BuildIndex {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.set("mapred.jar", "BuildIndex.jar");
+    //    conf.set("mapred.jar", "BuildIndex.jar");
         Job job = Job.getInstance(conf, "Inverted Index");
-     
+        job.setJarByClass(BuildIndex.class);
         job.setMapperClass(IndexMapper.class);
         job.setCombinerClass(IndexReducer.class);
         job.setReducerClass(IndexReducer.class);
@@ -56,7 +63,6 @@ public class BuildIndex {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setJarByClass(BuildIndex.class);
         job.waitForCompletion(true);
     }
 }
